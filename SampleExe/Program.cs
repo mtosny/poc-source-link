0754563b664a2ab10d19b228d1282a50e0def842
Mateusz Tosnowiec <mtosny@gmail.com>
2020-03-15 22:49:58 +0100

    SampleExe project using SampleLibrary as nuget package.

---
+++ SampleExe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleClassInstance = new SampleLibrary.SampleClass();
+            sampleClassInstance.ToLowerInvariant("qwerty");
+        }
+    }
+}
