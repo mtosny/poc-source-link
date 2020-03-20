1c73109250b3db27c93fb673877e1542b01e8319
Mateusz Tosnowiec <mtosny@gmail.com>
2020-03-20 17:40:19 +0100

    sample library v1.1

--- SampleLibrary/SampleClass.cs
+++ SampleLibrary/SampleClass.cs
@@ -1,10 +1,19 @@
-﻿namespace SampleLibrary
+﻿using System;
+
+namespace SampleLibrary
 {
     public class SampleClass
     {
         public string ToLowerInvariant(string text)
         {
             return text.ToLowerInvariant();
+        }
+
+        public string Inverse(string text)
+        {
+            char[] arr = text.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
         }
     }
 }
